67ac0bce268fc98a93b7ce98f3fea310e5737079
Asta1984 <salilmandal908@gmail.com>
2025-03-06 22:47:35 +0530

    jal

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AnimatedNavigationTabs } from './components/ui/animated-navigation-tabs';
 import Home from './pages/Home';
 import { Foooter } from './components/Foooter';
-import DashboardLayout from "./layouts/DashboardLayout";
-import Dashboard from "./pages/Dashboard";
-import DoctorDashboard from "./pages/DoctorDashboard";
-import PatientDashboard from "./pages/PatientDashboard";
-import Availability from "./pages/Availability";
-import Locations from "./pages/Locations";
-import Settings from "./pages/Settings";
-import AppointmentBooking from './pages/AppointmentBooking';
-import DoctorProfile from './pages/Profile';
-import DoctorSearch from './pages/DoctorSearch';
 
 
 const navigationItems = [
@@ -29,25 +19,6 @@ function App() {
           <Routes>
             {/* Home Route */}
             <Route path="/" element={<Home />} />
-
-            {/* Dashboard Routes */}
-            <Route path="dashboard" element={<DashboardLayout />}>
-              {/* Conditional Dashboard Home */}
-              <Route index element={<Dashboard />} />
-
-              {/* Doctor-specific Routes */}
-              <Route path="doctor" element={<DoctorDashboard />}>
-                <Route path="availability" element={<Availability />} />
-                <Route path="locations" element={<Locations />} />
-                <Route path="settings" element={<Settings />} />
-              </Route>
-
-              {/* Patient-specific Route */}
-              <Route path="patient" element={<PatientDashboard />} />
-              <Route path="booking" element={<AppointmentBooking />} />
-              <Route path="doctorprofile" element={<DoctorProfile />} />
-              <Route path="doctorsearch" element={<DoctorSearch/>} />
-            </Route>
           </Routes>
         </main>
       </div>
