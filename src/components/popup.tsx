ff41a5bab19e9e0d87eb0533be45b82b10b9b49a
Asta1984 <salilmandal908@gmail.com>
2025-06-05 12:57:50 +0530

    popup

--- src/components/popup.tsx
+++ src/components/popup.tsx
@@ -1,13 +1,36 @@
-import { X } from "lucide-react"
+import { useState } from "react";
+import { X } from "lucide-react";
+import { motion, AnimatePresence } from "framer-motion";
 
 export function Popup() {
-    return(
-        <>
-        <div className="bg-green-400 opacity-70 rounded md:h-dvh md:w-2xl">
-            <div className="p-0.5">
-                <X className="flex justify-self-end rounded bg-amber-600 hover:bg-orange-700 hover:scale-90"/>
-            </div>
-        </div>
-        </>
-    )
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 0.9, y: 0 }}
+          exit={{ opacity: 0, y: 50 }}
+          transition={{ duration: 0.4, ease: "easeInOut" }}
+          className="bg-green-400 rounded md:h-dvh md:w-2xl relative z-50"
+        >
+          <div className="p-0.5 flex justify-end">
+            <button onClick={() => setIsOpen(false)}>
+              <X className="rounded bg-amber-600 hover:bg-orange-700 hover:scale-90 transition-all" />
+            </button>
+          </div>
+
+          <div className="p-4">
+            <h2 className="text-white  text-xl">Demo Patient Credentials.</h2>
+            <p className="text-white">Email: Patient1@example.com</p>
+            <p className="text-white">Pass: password123</p>
+            <h2 className="text-white text-xl">Demo Doctor Credentials.</h2>
+            <p className="text-white">Email: Doctor1@example.com</p>
+            <p className="text-white">Pass: password123</p>
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
 }
